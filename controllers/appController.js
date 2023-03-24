1e53be9ff7d5954f5d69983dabe8607d299c95f5
chankaran2996 <chandrasekaran2996@gmail.com>
2023-03-25 02:39:27 +0530

    day11

---
+++ controllers/appController.js
@@ -0,0 +1,10 @@
+
+
+export const register = async (req,res) => {
+    res.json("Register success");
+}
+
+
+export const login = async (req,res) => {
+    res.json("login success");
+}
--- router/route.js
+++ router/route.js
@@ -4,18 +4,23 @@ const route = Router();
 
 
 // GET metods
+
+// user with username
 Router.route('/user/:username').get((req,res) => {
     res.json(" success");
 });
 
+// To generate random OTP
 Router.route('/generateOTP').get((req,res) => {
     res.json(" success");
 });
 
+// For verfiy OTP 
 Router.route('/verfiyOTP').get((req,res) => {
     res.json(" success");
 });
 
+// rest all varabiles 
 Router.route('/createReset').get((req,res) => {
     res.json(" success");
 });
@@ -24,25 +29,35 @@ Router.route('/createReset').get((req,res) => {
 
 // POST methods
 
-Router.route('/register').post((req,res) => {
+// For updating user data
+Router.route('/updateUser').post((req,res) => {
     res.json("register success");
 });
 
+// for sending mail
 Router.route('/registerMail').post((req,res) => {
     res.json("registerMail success");
 });
 
+// for autherntication 
 Router.route('/authenticate').post((req,res) => {
     res.json("authenticate success");
 });
 
+// for login 
 Router.route('/login').post((req,res) => {
     res.json("login success");
 });
 
 // PUT methods
 
+Router.route('/updateUser').put((req,res) => {
+    res.json("login success");
+});
 
+Router.route('/resetPassword').put((req,res) => {
+    res.json("login success");
+});
 
 
 export default route;
