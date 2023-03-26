e4be9d66549311990236eada1af9eb9e51c6b6b3
swathySuriyababu12 <swathysuryababu@gmail.com>
2023-03-26 22:16:07 +0530

    first_commit

--- database/connection.js
+++ database/connection.js
@@ -1,19 +1,16 @@
-// importing mongoose to connect with mongoDB
 import mongoose from "mongoose";
-// connceting with mongoDB 
+
 const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        useCreateIndex: true, 
-        useFindAndModify: false, 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-      });
-      console.log("connected to MONGODB");
-    } catch (err) {
-      throw err;
-      process.exit();
-    }
-  };
-//  exporting the connection
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to MONGODB");
+  } catch (err) {
+    throw err;
+    process.exit();
+  }
+};
+
 export default connectDB;
--- index.js
+++ index.js
@@ -1,50 +1,46 @@
 // Importing express
 import express from "express";
-// Improting cors which handile cors policies 
+// Improting cors which handile cors policies
 import cors from "cors";
 // Importing morgan
 import morgan from "morgan";
-// imporing DB connection 
-import connectDB from "../database/connection.js";
-// importing route connection 
+import dotenv from "dotenv";
+dotenv.config();
+// imporing DB connection
+import connectDB from "./database/connection.js";
+// importing route connection
 import route from "./router/route.js";
 // const user = require('./userModel.js')
 // Creating object for expresss
 const app = express();
-// For allowing other orgin 
+// For allowing other orgin
 app.use(cors());
 //  It parses incoming requests with JSON payloads and is based on body-parser
 app.use(express.json());
 
-app.use(morgan('tiny'));
-app.disable('x-powered-by');
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
 
-// connnection 
+// connnection
 connectDB();
 
-// creating responce for home routs 
-app.get('/',(req,res) => {
-    res.status(200).json("Sucessfully connected")
-})
+// creating responce for home routs
+app.get("/", (req, res) => {
+  res.status(200).json("Sucessfully connected");
+});
 
+// connecting with api route
+app.use("/api", route);
 
-// connecting with api route 
-app.use('/api',route);
-
-
-
-
-
-
-//  starting server 
-app.listen(8000 ,() =>{
-    console.log("sever sarted at 8000")
-})
+//  starting server
+app.listen(8000, () => {
+  console.log("sever sarted at 8000");
+});
 
 // const connectionParams={
-//     useCreateIndex: true, 
-//     useFindAndModify: false, 
-//     useNewUrlParser: true, 
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // }
 // mongoose.connect('mongodb+srv://chan2996:chan2996@cluster0.zfadu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -70,7 +66,7 @@ app.listen(8000 ,() =>{
 //     } catch (error) {
 //         res.json({status:'error',error:error})
 //     }
-    
+
 // })
 
 // app.post('/api/login', async (req,res) => {
