73cca5a0b003e1b9ee1c6d53d62440876e773bbb
swathySuriyababu12 <swathysuryababu@gmail.com>
2023-03-26 22:31:29 +0530

    mongo error resolved

--- router/route.js
+++ router/route.js
@@ -1,66 +1,58 @@
-import { Router } from "express";
+import express from "express";
+const router = express.Router();
+import { register } from "../controllers/appController.js";
+
 // importing all controlles
-import * as controller from '../controllers/appController.js';
-
-
-const route = Router();
-
 
 // GET metods
-
+router.get("/register", register);
 // user with username
-Router.route('/user/:username').get((req,res) => {
-    res.json(" success");
-});
 
 // To generate random OTP
-Router.route('/generateOTP').get((req,res) => {
-    res.json(" success");
-});
+// Router.route("/generateOTP").get((req, res) => {
+//   res.json(" success");
+// });
 
-// For verfiy OTP 
-Router.route('/verfiyOTP').get((req,res) => {
-    res.json(" success");
-});
+// // For verfiy OTP
+// Router.route("/verfiyOTP").get((req, res) => {
+//   res.json(" success");
+// });
 
-// rest all varabiles 
-Router.route('/createReset').get((req,res) => {
-    res.json(" success");
-});
+// // rest all varabiles
+// Router.route("/createReset").get((req, res) => {
+//   res.json(" success");
+// });
 
+// // POST methods
 
+// // For updating user data
+// Router.route("/register").post((req, res) => {
+//   res.json("register success");
+// });
 
-// POST methods
+// // for sending mail
+// Router.route("/registerMail").post((req, res) => {
+//   res.json("registerMail success");
+// });
 
-// For updating user data
-Router.route('/register').post((req,res) => {
-    res.json("register success");
-});
+// // for autherntication
+// Router.route("/authenticate").post((req, res) => {
+//   res.json("authenticate success");
+// });
 
-// for sending mail
-Router.route('/registerMail').post((req,res) => {
-    res.json("registerMail success");
-});
+// // for login
+// Router.route("/login").post((req, res) => {
+//   res.json("login success");
+// });
 
-// for autherntication 
-Router.route('/authenticate').post((req,res) => {
-    res.json("authenticate success");
-});
+// // PUT methods
 
-// for login 
-Router.route('/login').post((req,res) => {
-    res.json("login success");
-});
+// Router.route("/updateUser").put((req, res) => {
+//   res.json("login success");
+// });
 
-// PUT methods
+// Router.route("/resetPassword").put((req, res) => {
+//   res.json("login success");
+// });
 
-Router.route('/updateUser').put((req,res) => {
-    res.json("login success");
-});
-
-Router.route('/resetPassword').put((req,res) => {
-    res.json("login success");
-});
-
-
-export default route;
+export default router;
